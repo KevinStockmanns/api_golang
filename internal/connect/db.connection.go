3aa50180c11e09c7b14491cf3aeda8764ccf0303
Kevin <stockmannskevin@gmail.com>
2024-09-29 19:33:48 -0300

    feat: new table´s db

--- internal/connect/db.connection.go
+++ internal/connect/db.connection.go
@@ -3,6 +3,7 @@ package connect
 import (
 	"log"
 
+	"github.com/KevinStockmanns/api_golang/internal/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,11 +11,14 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(127.0.0.1:3307)/api_go?charset=utf8mb4&parseTime=True&loc=UTC"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Println("Error al conectar a la base de datos")
 		panic(err)
 	}
+}
+func InitMigrations() {
+	DB.AutoMigrate(models.User{})
 }
