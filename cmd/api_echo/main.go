3e5eb8bbd2f5de90d2e0fbfd02d255c6516721d9
Kevin <stockmannskevin@gmail.com>
2024-09-29 19:51:51 -0300

    connecting modulares

--- cmd/api_echo/main.go
+++ cmd/api_echo/main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"log"
 
+	v1 "github.com/KevinStockmanns/api_golang/api/v1"
 	"github.com/KevinStockmanns/api_golang/internal/db"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	log.Println("Listening...")
-	e := echo.New()
-
 	db.ConnectDB()
+	log.Println("DB Connected")
 	db.InitMigrations()
+	log.Println("Listening...")
 
+	e := echo.New()
+	v1.RegisterRoutes(e)
 	if err := e.Start(":8080"); err != nil {
 		log.Println("ocurrio un error al levantar el servidor")
 		log.Println(err)
--- internal/models/user.model.go
+++ internal/models/user.model.go
@@ -6,8 +6,8 @@ type User struct {
 	ID       uint      `json:"id" gorm:"primaryKey"`
 	Name     string    `json:"name" gorm:"type:varchar(50)"`
 	LastName string    `json:"lastname" gorm:"type:varchar(50)"`
-	Email    string    `json:"email" gorm:"type:varchar(255),unique"`
-	Password string    `json:"password" gorm:"varchar(255)"`
+	Email    string    `json:"email" gorm:"type:varchar(255);unique"`
+	Password string    `json:"password" gorm:"type:varchar(255)"`
 	Birthday time.Time `json:"birthday" gorm:"type:date"`
 	Status   bool      `json:"status"`
 }
