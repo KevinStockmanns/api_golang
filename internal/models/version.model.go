249793e81ab8922e005e4ad12b351a77a25652b3
Kevin <stockmannskevin@gmail.com>
2024-10-05 18:23:43 -0300

    refactor: normalize product

--- internal/models/version.model.go
+++ internal/models/version.model.go
@@ -5,14 +5,15 @@ import (
 	"time"
 
 	"github.com/KevinStockmanns/api_golang/internal/dtos"
+	"github.com/KevinStockmanns/api_golang/internal/utils"
 )
 
 type Version struct {
 	ID          uint `gorm:"primaryKey"`
 	ProductId   uint
-	Name        string `gorm:"varchar(50)"`
-	Price       float64
-	ResalePrice *float64
+	Name        string   `gorm:"varchar(50)"`
+	Price       float64  `gorm:"type:decimal(10,2)"`
+	ResalePrice *float64 `gorm:"type:decimal(10,2)"`
 	Status      bool
 	Date        time.Time
 	Stock       uint
@@ -25,7 +26,7 @@ func (v *Version) Create(data dtos.VersionCreateDTO) {
 	v.Price = data.Price
 	v.ResalePrice = data.ResalePrice
 	v.Status = data.Status
-	v.Stock = v.Stock
+	v.Stock = data.Stock
 	v.Views = 0
 
 	v.Normalize()
@@ -33,4 +34,8 @@ func (v *Version) Create(data dtos.VersionCreateDTO) {
 
 func (v *Version) Normalize() {
 	v.Name = strings.ToTitle(strings.TrimSpace(v.Name))
+	v.Price = utils.RoundDecimal(v.Price)
+	if v.ResalePrice != nil {
+		*v.ResalePrice = utils.RoundDecimal(*v.ResalePrice)
+	}
 }
--- internal/utils/util.go
+++ internal/utils/util.go
@@ -1,8 +1,15 @@
 package utils
 
-import "regexp"
+import (
+	"math"
+	"regexp"
+)
 
 func IsInteger(num string) bool {
 	matched, _ := regexp.MatchString("^[0-9]+$", num)
 	return matched
+}
+
+func RoundDecimal(decimal float64) float64 {
+	return (math.Ceil(decimal*10) / 10)
 }
