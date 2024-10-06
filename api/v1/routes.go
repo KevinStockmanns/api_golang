5cc635e6f97ee62466f0e0dd0a869ff730613044
Kevin <stockmannskevin@gmail.com>
2024-10-06 11:29:33 -0300

    refactor: allowed roles

--- api/v1/routes.go
+++ api/v1/routes.go
@@ -10,16 +10,20 @@ import (
 func RegisterRoutes(e *echo.Echo) {
 	v1 := e.Group("/v1")
 
+	admin := string(constants.Admin)
+	superAdmin := string(constants.SuperAdmin)
+	admins := []string{admin, superAdmin}
+
 	// User´s Endpoints
 	v1.POST("/user/signup", handlers.UserSignUp)
 	v1.POST("/user/login", handlers.UserLogin)
 	v1.PUT("/user/:id", handlers.UserUpdate, middlewares.JwtMiddleware())
 	v1.GET("/user/:id", handlers.GetUser, middlewares.JwtMiddleware())
-	v1.GET("/user", handlers.UserList, middlewares.JwtMiddleware(string(constants.SuperAdmin), string(constants.Admin)))
+	v1.GET("/user", handlers.UserList, middlewares.JwtMiddleware(admins...))
 	v1.PUT("/user/password", handlers.UserChangePassword, middlewares.JwtMiddleware())
 
 	//Product´s Endpoints
-	v1.POST("/product", handlers.ProductPostHandler, middlewares.JwtMiddleware(string(constants.SuperAdmin), string(constants.Admin)))
+	v1.POST("/product", handlers.ProductPostHandler, middlewares.JwtMiddleware(admins...))
 	v1.GET("/product/:id", handlers.GetUser)
-	v1.PATCH("product/views", handlers.ProductViews, middlewares.JwtMiddleware(string(constants.Admin), string(constants.SuperAdmin)))
+	v1.PATCH("product/views", handlers.ProductViews, middlewares.JwtMiddleware(admins...))
 }
